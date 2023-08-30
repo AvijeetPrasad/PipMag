3bf4a43915f40adada8f06b3124e7f13aa4a1235
Avijeet Prasad <prasad.avijeet@gmail.com>
2023-08-30 08:15:34 +0200

    [DEV] Refactor TestReadAndFormatCSV for Portability and Cleanup
    - Added `tempfile` module to handle temporary file and directory creation
      in a cross-platform manner (Lines 4, 9-11).
    
    - Replaced hard-coded paths ("/mnt/data/sample.csv") with dynamic paths
      generated using `tempfile` and `os.path.join` (Lines 18-21, 28-33, 37-42,
      46-49).
    
    - Updated `tearDown` method to use `temp_dir.cleanup()` for automatic
      temporary directory deletion (Lines 27-28).
    
    - Modified test cases to use the new dynamic file paths, ensuring
      compatibility across macOS, Linux, and other Unix-like systems (Lines 35,
      41, 47, 53).
    
    This commit enhances the code quality by making it more robust and portable.

--- tests/test_file_utils.py
+++ tests/test_file_utils.py
@@ -1,39 +1,44 @@
 import unittest
 import os
 import pandas as pd
+import tempfile
 from pipmag.file_utils import read_and_format_csv
 
 class TestReadAndFormatCSV(unittest.TestCase):
 
     def setUp(self):
+        # Create a temporary directory using the tempfile module
+        self.temp_dir = tempfile.TemporaryDirectory()
+
         # Sample test data for demonstration
         self.sample_csv_data = """date_time,year,month,day,time,instruments,target,comments,video_links,image_links,links,num_links,polarimetry
         2013-06-30 09:15:50,2013,6,30,09:15:50,CRISP;IRIS,Spicules,,http://example.com/video,http://example.com/image,http://example.com/links,3,False
         """
-        # Create a temporary CSV file for testing
-        with open("/mnt/data/sample.csv", "w") as f:
+
+        # Create a temporary CSV file within the temporary directory
+        self.sample_csv_file = os.path.join(self.temp_dir.name, 'sample.csv')
+        with open(self.sample_csv_file, "w") as f:
             f.write(self.sample_csv_data)
 
     def tearDown(self):
-        # Delete temporary files
-        os.remove("/mnt/data/sample.csv")
-        if os.path.exists("/mnt/data/empty.csv"):
-            os.remove("/mnt/data/empty.csv")
+        # Cleanup the temporary directory
+        self.temp_dir.cleanup()
 
     def test_file_not_found(self):
-        self.assertEqual(read_and_format_csv("/mnt/data/nonexistent.csv"), "Error: The specified file was not found.")
+        self.assertEqual(read_and_format_csv("nonexistent.csv"), "Error: The specified file was not found.")
         
     def test_empty_file(self):
-        with open("/mnt/data/empty.csv", "w") as f:
+        empty_file_path = os.path.join(self.temp_dir.name, 'empty.csv')
+        with open(empty_file_path, "w") as f:
             f.write("")
-        self.assertEqual(read_and_format_csv("/mnt/data/empty.csv"), "Error: The file is empty.")
+        self.assertEqual(read_and_format_csv(empty_file_path), "Error: The file is empty.")
     
     def test_incorrect_columns(self):
-        self.assertEqual(read_and_format_csv("/mnt/data/sample.csv", expected_columns=['nonexistent_column']), 
-                         "Error: Missing one or more expected columns. Expected: ['nonexistent_column'], Found: ['date_time', 'year', 'month', 'day', 'time', 'instruments', 'target', 'comments', 'video_links', 'image_links', 'links', 'num_links', 'polarimetry']")
+        self.assertEqual(read_and_format_csv(self.sample_csv_file, expected_columns=['nonexistent_column']), 
+                         "Error: Missing expected columns. Expected: ['nonexistent_column'], Found: ['date_time', 'year', 'month', 'day', 'time', 'instruments', 'target', 'comments', 'video_links', 'image_links', 'links', 'num_links', 'polarimetry']")
         
     def test_correct_file(self):
-        df = read_and_format_csv("/mnt/data/sample.csv")
+        df = read_and_format_csv(self.sample_csv_file)
         self.assertTrue(isinstance(df, pd.DataFrame))
         self.assertTrue(pd.to_datetime("2013-06-30 09:15:50") == df['date_time'][0])
         self.assertTrue(df['instruments'][0] == ['CRISP', 'IRIS'])
